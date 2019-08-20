f745311fd80180a10633b3f782aba0566bb272a8
Randy Park <randyp@smu.edu>
2019-08-20 01:31:24 -0400

    initial version

---
+++ DataStructure/MovieRatingPrediction.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieRecommend.DataStructures
+{
+    class MovieRatingPrediction
+    {
+        public float Label;
+
+        public float Score;
+    }
+}
